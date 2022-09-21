6678f4f7c116c17a1336ed4ddfb226378e6ac32c
marijara99 <marijarasojo@gmail.com>
2022-09-21 11:21:11 +0200

    Todos los tests funcionan

--- src/main/java/org/example/WhatMultiple.java
+++ src/main/java/org/example/WhatMultiple.java
@@ -1,35 +1,30 @@
 package org.example;
-
 import java.util.Scanner;
 
 public class WhatMultiple {
-    private Integer number;
+    // declares an Array of integers.
+    double[] myDoubleArray = new double[]{10000, 20073, 5, 3, 15, 567, 7262};
+    double number;
+
     public String result;
 
-    
+    public String WhatDoesItReturn(Integer number) {
 
-    public String WhatDoesItReturn() {
+            if (number % 15 == 0) {
+                result = "FizzBuzz";
+            } else if (number % 5 == 0) {
+                result = "Buzz";
 
-        number = -1;
-        while (number < 0 || number > 100) {
-            System.out.println("Introduce un numero entre 0 y 100");
-            Scanner sc = new Scanner(System.in);
-            number = sc.nextInt();
-        }
+            } else if (number % 3 == 0) {
+                result = "Fizz";
 
-        if (number % 15 == 0) {
-            result = "FizzBuzz";
-        } else if (number % 5 == 0) {
-            result = "Buzz";
+            } else {
+                result = number.toString();
+            }
 
-        } else if (number % 3 == 0) {
-            result = "Fizz";
-
-        } else {
-            result = "LightYear";
-        }
 
         return result;
+
 
     }
 }
--- src/test/java/org/example/WhatMultipleTest.java
+++ src/test/java/org/example/WhatMultipleTest.java
@@ -5,15 +5,44 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class WhatMultipleTest {
+public class WhatMultipleTest  {
 
     @Test
-    public void whatDoesItReturn() {
+    public void whatDoesItReturn1() {
         WhatMultiple wm = new WhatMultiple();
-        String expected = "FizzBuzz";
-        String result = wm.WhatDoesItReturn();
-
-        assertEquals(result, expected);
+        assertEquals("FizzBuzz", wm.WhatDoesItReturn(15));
 
     }
+    @Test
+    public void whatDoesItReturn2() {
+        WhatMultiple wm = new WhatMultiple();
+        assertEquals("16327", wm.WhatDoesItReturn(16327));
+
+    }
+    @Test
+    public void whatDoesItReturn3() {
+        WhatMultiple wm = new WhatMultiple();
+        assertEquals("FizzBuzz", wm.WhatDoesItReturn(3525));
+
+    }
+    @Test
+    public void whatDoesItReturn4() {
+        WhatMultiple wm = new WhatMultiple();
+        assertEquals("Buzz", wm.WhatDoesItReturn(320));
+
+    }
+    @Test
+    public void whatDoesItReturn5() {
+        WhatMultiple wm = new WhatMultiple();
+        assertEquals("Buzz", wm.WhatDoesItReturn(69595));
+
+    }
+    @Test
+    public void whatDoesItReturn6() {
+        WhatMultiple wm = new WhatMultiple();
+        assertEquals("Fizz", wm.WhatDoesItReturn(513));
+
+    }
+
+
 }
