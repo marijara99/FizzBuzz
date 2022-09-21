08d7d27ba8beff67bad752029886f74dfe789ac5
marijara99 <marijarasojo@gmail.com>
2022-09-21 10:01:05 +0200

    primer metodo

--- src/main/java/org/example/WhatMultiple.java
+++ src/main/java/org/example/WhatMultiple.java
@@ -1,11 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class WhatMultiple {
-    private int number;
+    private Integer number;
+    private String result;
 
     public WhatMultiple(int number) {
         this.number = number;
     }
 
-    public String
+    public String WhatDoesItReturn() {
+
+        number = -1;
+        while (number < 0 || number > 100) {
+            System.out.println("Introduce un numero entre 0 y 100");
+            Scanner sc = new Scanner(System.in);
+            number = sc.nextInt();
+
+
+        }
+
+        if (number % 15 == 0) {
+            result = "FizzBuzz";
+        } else if (number % 5 == 0) {
+            result = "Buzz";
+
+        } else if (number % 3 == 0) {
+            result = "Fizz";
+
+        } else {
+            result = "LightYear";
+        }
+
+        return result;
+
+    }
 }
+
